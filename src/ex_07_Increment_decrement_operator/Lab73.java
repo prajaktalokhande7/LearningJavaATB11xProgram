fbc4ad60a39f135a253fa1eac4b8dff0e199ce8f
prajaktalokhande7 <prajaktaplokhande@gmail.com>
2025-03-14 17:06:40 +0530

    12th and 13th march task

---
+++ src/ex_07_Increment_decrement_operator/Lab73.java
@@ -0,0 +1,12 @@
+package ex_07_Increment_decrement_operator;
+
+public class Lab73 {
+    public static void main(String[] args) {
+        int a= 10;
+        System.out.println(a++ + ++a);
+        int b = 10;
+        System.out.println(++b + ++b);
+        System.out.println(b);
+    }
+
+}
---
+++ src/ex_08_Condition/Lab79_interview.java
@@ -0,0 +1,13 @@
+package ex_08_Condition;
+
+public class Lab79_interview {
+    public static void main(String[] args) {
+        int a = 10;
+        if ( a == 4){
+            System.out.println("a");
+        }
+        else{
+            System.out.println("b");
+        }
+    }
+}
